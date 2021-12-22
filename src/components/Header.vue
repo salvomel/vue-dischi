acd9bb48bcd612e2f7c820eba881f1fc5fe4bdd4
salvomel <salv.mel@gmail.com>
2021-12-22 15:07:15 +0100

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <img src="../assets/logo-small.svg" alt="Logo Spotify">
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/variables';
+
+header {
+    background-color: $secondary-color;
+    
+   img {
+       height: 50px;
+       padding: 10px 20px;
+   }
+}
+</style>
