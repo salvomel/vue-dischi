572afbe2763232fa6cbf5b5e51dfd6189f8f7269
salvomel <salv.mel@gmail.com>
2022-01-10 16:29:18 +0100

    add genre filter

--- src/components/AlbumList.vue
+++ src/components/AlbumList.vue
@@ -1,10 +1,13 @@
 <template>
     <main>
+        <Select @selectClicked="searchGenre" />
         <div class="container">
             <div class="main-wrapper">
-                <div v-if="albums.length > 0" class="row row-cols-md-2 row-cols-lg-4 row-cols-xl-5">
-                    <AlbumCard v-for="(album, index) in albums" :key="index" :details="album" />
+
+                <div v-if="!isLoadingApi" class="row row-cols-md-2 row-cols-lg-4 row-cols-xl-5">
+                    <AlbumCard v-for="(album, index) in filteredAlbums" :key="index" :details="album" />
                 </div>
+
                 <Loader v-else />
             </div>
         </div>
@@ -14,23 +17,50 @@
 <script>
 import axios from 'axios';
 import AlbumCard from './AlbumCard';
+import Select from './Select';
 import Loader from './Loader';
 
 export default {
     name: 'AlbumList',
     components: {
         AlbumCard,
+        Select,
         Loader
     },
     data: function() {
         return {
             albums: [],
+            selectedGenre: 'All',
+            isLoadingApi: true
         };
+    },
+    methods: {
+        searchGenre: function(select) {
+            this.selectedGenre = select;
+        },
+    },
+    computed: {
+        filteredAlbums: function() {
+
+            // Se selezionato 'All' restituisco tutto l'array di albums
+            if(this.selectedGenre === 'All') {
+                return this.albums;
+            }
+
+            // Altrimenti restituisco la selezione filtrata
+            const filteredArray = this.albums.filter((element) => {
+                return element.genre.includes(this.selectedGenre);
+            });
+
+            return filteredArray;
+        }
     },
     created: function() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
         .then((response) => {
             this.albums = response.data.response;
+
+            this.isLoadingApi = false;
         });
     }
 }
@@ -40,7 +70,7 @@ export default {
     main {
 
         .main-wrapper {
-            padding: 70px 120px;
+            padding: 0 120px;
         }
     }
 </style>
---
+++ src/components/Select.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="select-wrapper">
+            <select @change="$emit('selectClicked', selectValue)" v-model="selectValue" class="form-select" aria-label="Default select example">
+                <option value="All">Tutti i generi</option>
+                <option value="Pop">Pop</option>
+                <option value="Rock">Rock</option>
+                <option value="Jazz">Jazz</option>
+                <option value="Metal">Metal</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Select",
+    data: function () {
+        return {
+            selectValue: 'All'
+        };
+    },
+}
+</script>
+
+<style scoped lang="scss">
+.select-wrapper {
+    
+    padding: 40px 120px;
+}
+</style>
